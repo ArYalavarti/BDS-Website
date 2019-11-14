77099fd749c821877bb1b01ed5934d8843cb722d
Arvind Yalavarti <yalavartiarvind@gmail.com>
2019-11-14 14:33:27 -0500

    Add Tippy and disabled workshop tooltip

--- js/rendering/events.js
+++ js/rendering/events.js
@@ -1,10 +1,15 @@
-workshops = [
+let disabledTooltips;
+
+UPCOMING_EVENTS = [];
+
+PAST_EVENTS = [
     {
         "name": "Pandas and Exploratory Data Analysis",
         "image": "../images/workshops/pandas-workshop.jpg",
         "event_link": "https://www.facebook.com/events/528263211064252/",
         "materials_link": "",
         "description": "Learn more about the fundamentals of the Pandas library and about Exploratory Data Analysis. By the end of the workshop, you will have learned how to read in different data files, search/analyze data of your own, get the “big picture” insights into datasets, and create visualizations",
+        "alt_btn": "",
         "workshop": true
     },
     {
@@ -19,42 +24,87 @@ workshops = [
 ];
 
 
-$(document).ready(function (){
-    for (let i = 0; i < workshops.length; i++) {
-        let item = workshops[i];
-        let img = "";
-        if (item.image !== "") {
-            img =  `<img class="workshop-img" src="${item.image}" />`
-        }
-        let btns = "";
-        if (item.workshop) {
-            btns = `<div>
-                        <a href="${item.event_link}" class="standard-btn">
-                        Facebook Event
-                        </a>
-                        <a href="${item.materials_link}" class="standard-btn">
-                        Workshop Materials
-                        </a>
-                    </div>`
-        } else {
-            btns = `<div>
-                        <a href="${item.event_link}" class="standard-btn">
-                            ${item.alt_btn}
-                        </a>`
-        }
+$(document).ready(function () {
+    (UPCOMING_EVENTS.length === 0) ? $("#inProgress").show() : renderWorkshops("upcomingEvents", UPCOMING_EVENTS);
+    renderWorkshops("pastEvents", PAST_EVENTS);
 
-
-        let generatedHTML = `<div class="wide-container work-container w-container">
-                                <h4 class="navy-text-block">${item.name}</h4>
-                                <img class="divider-small" src="../images/main/divider-small-navy.png"><br/>
-                                <p class="navy-text left event-text">
-                                    ${item.description}
-                                </p>
-                                <div class="center-container">
-                                    ${img}${btns}  
-                                </div>
-                            </div>`;
-        let html = $.parseHTML(generatedHTML);
-        $("#workshops").append(html);
-    }
+    initTooltips();
 });
+
+
+/**
+ * Initializes all given events on page under the provided div
+ */
+function renderWorkshops(id, workshops) {
+    for (let workshop of workshops) {
+        $(`#${id}`).append(generateEventHTML(workshop));
+    }
+}
+
+
+/**
+ * Generate the event HTML required to render event on screen
+ */
+function generateEventHTML(item) {
+    let img = "";
+    if (item.image !== "") {
+        img =  `<img class="workshop-img" src="${item.image}" />`
+    }
+    let btns = "";
+    if (item.workshop) {
+        let eventClass = "standard-btn";
+        let tippyContent = "";
+        let href = "";
+        if (item.materials_link === "") {
+            eventClass = "standard-btn workshop-disabled";
+            tippyContent = "Materials have not been released yet";
+        } else {
+            href = `href="${item.materials_link}"`;
+        }
+        btns = `<div>
+                <a href="${item.event_link}" class="standard-btn">
+                Facebook Event
+                </a>
+                <a ${href} class="${eventClass}"
+                data-tippy-content="${tippyContent}"
+                >
+                Workshop Materials
+                </a>
+                </div>`
+    } else {
+        btns = `<div>
+                <a href="${item.event_link}" class="standard-btn">
+                    ${item.alt_btn}
+                </a>
+                </div>`
+    }
+
+    let generatedHTML = `<div class="wide-container work-container w-container">
+                            <h4 class="navy-text-block">${item.name}</h4>
+                            <img class="divider-small" src="../images/main/divider-small-navy.png"><br/>
+                            <p class="navy-text left event-text">
+                                ${item.description}
+                            </p>
+                            <div class="center-container">
+                                ${img}${btns}  
+                            </div>
+                        </div>`;
+    return $.parseHTML(generatedHTML);
+}
+
+
+/**
+ * Initializes the page tooltips
+ */
+function initTooltips() {
+    disabledTooltips = tippy(".workshop-disabled", {
+        animation: "scale",
+        arrow: true,
+        arrowType: "round",
+        theme: "bds",
+        hideOnClick: true,
+        inertia: true,
+        sticky: true,
+        placement: "bottom",
+    });
+}
